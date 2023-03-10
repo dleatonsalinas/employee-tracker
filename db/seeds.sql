3a8cfebb541fb6dbfa172c37ef89901596316149
dleatonsalinas <daniellesalinas721@gmail.com>
2023-03-09 22:21:35 -0600

    add seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO employee_tracker (id, first_name, last_name, title, department_name, salary, manager_id)
+VALUES (1, "John", "Doe", "Manager", "Operations", 200000, NULL),
+        (2, "Mike", "Chan", "Salesperson", "Sales", 80000, "John Doe"),
+        (3, "Ashley", "Rodriguez", "Lead Engineer", "Engineering", 150000, NULL)
+       
